b0f69548b6c175f6e72ac7d8247ec02bb144b9cd
Minh Nguyen <draculedolar0763@gmail.com>
2025-09-28 03:37:19 +0700

    update sameSite (fax -> none) (vercel vs onrender)

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -61,7 +61,7 @@ const login = async (req, res) => {
   res.cookie('token', accessToken, {
     httpOnly: true,
     secure: true, // nếu dùng HTTPS thì để true
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000 // 1 ngày
   })
   res.json({ message: 'Login successful' })
