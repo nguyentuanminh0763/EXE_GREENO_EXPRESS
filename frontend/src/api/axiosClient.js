6829541ce78dd005f4b3d58c361c276c3a0f0a62
Minh Nguyen <draculedolar0763@gmail.com>
2025-09-28 03:08:03 +0700

    update axiosClient to deploy (main)

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -14,7 +14,7 @@ import { useNavigate } from 'react-router-dom';
 // });
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://exe-greeno-express.onrender.com/api',
   withCredentials: true // 💡 Phải có dòng này để gửi cookie
 });
 
