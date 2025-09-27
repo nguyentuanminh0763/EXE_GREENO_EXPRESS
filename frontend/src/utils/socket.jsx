160ad39bcbe16e7207c8609583d6d3b63c0d70fe
Minh Nguyen <draculedolar0763@gmail.com>
2025-09-28 03:12:07 +0700

    UPDATE SOCKET

--- frontend/src/utils/socket.jsx
+++ frontend/src/utils/socket.jsx
@@ -1,8 +1,9 @@
-// src/utils/socket.jsx
-import { io } from "socket.io-client";
+// // src/utils/socket.jsx
+// import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
-  withCredentials: true
-});
+// const socket = io("http://localhost:3000", {
+//   withCredentials: true
+// });
 
-export default socket;
+// export default socket;
+// // 
